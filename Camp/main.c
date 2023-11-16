2c8b345dad58dac9c00b04eda69478f85c3a1399
Kim Bum Jun <admin@vivabj.com>
2023-11-16 19:58:33 +0900

    Initial Commit

---
+++ Camp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Camp
+//
+//  Created by 김범준 on 11/16/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
